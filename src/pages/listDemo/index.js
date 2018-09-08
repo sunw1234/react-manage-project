b48769338a5ebd72bf9a9e2715bbebf97078a0fb
sunw <1156845285@qq.com>
2018-09-08 15:00:37 +0800

    list

--- src/pages/listDemo/index.js
+++ src/pages/listDemo/index.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import { Table, Divider, Tag } from 'antd';
+import './index.less';
+
+const data = [{
+	key: '1',
+	name: 'John Brown',
+	age: 32,
+	address: 'New York No. 1 Lake Park',
+	tags: ['nice', 'developer'],
+}, {
+	key: '2',
+	name: 'Jim Green',
+	age: 42,
+	address: 'London No. 1 Lake Park',
+	tags: ['loser'],
+}, {
+	key: '3',
+	name: 'Joe Black',
+	age: 32,
+	address: 'Sidney No. 1 Lake Park',
+	tags: ['cool', 'teacher'],
+}];
 
 class ListDemo extends React.Component {
     render(){
+			const columns = [{
+				title: 'Name',
+				dataIndex: 'name',
+				key: 'name',
+				render: text => <span>{text}</span>,
+			}, {
+				title: 'Age',
+				dataIndex: 'age',
+				key: 'age',
+			}, {
+				title: 'Address',
+				dataIndex: 'address',
+				key: 'address',
+			}, {
+				title: 'Tags',
+				key: 'tags',
+				dataIndex: 'tags',
+				render: tags => (
+					<span>
+						{tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+					</span>
+				),
+			}, {
+				title: 'Action',
+				key: 'action',
+				render: (text, record) => (
+					<span>
+						<Divider type="vertical" />
+					</span>
+				),
+			}];
+		
         return(
-            <div>list页面</div>
+            <div className="list-page-wrap">list页面
+							<Table columns={columns} dataSource={data} />
+						</div>
         )
     }
 } 
